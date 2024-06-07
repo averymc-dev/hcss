fd4d27eebc5b958c6c0171d7172444d52082bca7
averymc-dev <averymc.dev@gmail.com>
2024-06-07 09:55:48 -0500

    first commit

---
+++ functions/fetch-data/fetch-data.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+
+const handler = async (event) => {
+  const unknown = event.queryStringParameters
+  const API_KEY = process.env.API_KEY
+  const url = `http//api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+  try {
+    const { data } = await axios.get(url)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (error){
+    const { status, statusText, headers, data } = error.response
+    return {
+      statusCode: status,
+      body: JSON.stringify({status, statusText, headers, data})
+    }
+  }
+}
+
+module.exports = { handler }
