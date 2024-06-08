e1e81fad02ea58ac9be810caeeb2d6ab86fc9d73
averymc-dev <averymc.dev@gmail.com>
2024-06-08 16:12:36 -0500

    capture elements as variables, display data to elements

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,14 @@ fetch(`/.netlify/functions/fetch-data`)
     displayInfo(data)
   })
 
+let date = document.getElementById('date')
+let title = document.getElementById('photoTitle')
+let img = document.getElementById('img')
+let expl = document.getElementById('expl')
 
 function displayInfo(data) {
-  console.log(data)
-  document.getElementById('photoTitle').innerText = data.title
+  date.innerText = data.date
+  title.innerText = data.title
+  img.src = data.url
+  expl.innerText = data.explanation
 }
