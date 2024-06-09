b8c2680f98f30c5272de46db00f53fb08c0c8fbd
averymc-dev <averymc.dev@gmail.com>
2024-06-09 14:59:07 -0500

    Modified function to format and render date

--- src/js/main.js
+++ src/js/main.js
@@ -20,5 +20,16 @@ function formatDate(date) {
   date = date.split('-')
   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   let [year, month, day] = [ ...date ]
-  return `${months[month - 1]} ${day}, ${year}`
+  let dateSuffix = 'th'
+  if(day < 10){
+    day = day.split('0').slice(1)
+    if(day == 1){
+      dateSuffix = 'st'
+    }else if(day == 2){
+      dateSuffix = 'nd'
+    }else if (day == 3){
+      dateSuffix = 'rd'
+    }
+  }
+  return `${months[month - 1]} ${day}${dateSuffix}, ${year}`
 }
